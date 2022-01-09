a72c138554c296b57a5a36079c8ad4a6612aae84
Hamzsd <hamza_sd@outlook.com>
2022-01-09 03:05:51 +0100

    add graphql sample

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useQuery, gql } from '@apollo/client';
+
+const FEED_QUERY = gql`{
+  user(id: "4dc70521-22bb-4396-b37a-4a927c66d43b")  {
+      id
+      email
+      name
+  }
+}`;
+
 export default function App() {
+  const { loading, error, data } = useQuery(FEED_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="container">
+      <h1 className="text-3xl font-bold underline">Hamza</h1>
+      <div className="row">
+        <h2>UserID: {data.user.id}</h2>
+      </div>
+    </div>
+
+
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.mocki.io/v2/c4d7a195/graphql'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <React.StrictMode>
+     <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
