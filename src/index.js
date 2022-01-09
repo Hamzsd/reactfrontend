64ef87d4880f7ba9664a9981a5bd11e6ed8eca55
Hamzsd <hamza_sd@outlook.com>
2022-01-09 14:30:45 +0100

    add table react

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
-  InMemoryCache
-} from '@apollo/client';
+  InMemoryCache,
+} from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: 'https://api.mocki.io/v2/c4d7a195/graphql'
+  uri: "https://api.mocki.io/v2/c4d7a195/graphql",
 });
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <React.StrictMode>
-     <ApolloProvider client={client}>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
